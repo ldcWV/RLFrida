54c303520c96fa9940d5a5edbafa730e988bdd35
ldcWV <ldorianchen@gmail.com>
2024-05-13 15:24:29 -0400

    Renderer abstract class

---
+++ renderers/renderer.py
@@ -0,0 +1,9 @@
+class Renderer:
+    def __init__(self):
+        pass
+
+    def draw(self, trajectory):
+        raise NotImplementedError
+
+    def clear(self):
+        raise NotImplementedError
