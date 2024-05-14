b09263a707b4935ca48cb556c754b81fa5e86b88
ldcWV <ldorianchen@gmail.com>
2024-05-14 14:57:36 -0400

    reorganize so that differentiable renderers dont need to be trained

--- src/renderers/cairo_path_renderer.py
+++ src/renderers/cairo_path_renderer.py
@@ -3,14 +3,16 @@ import numpy as np
 import cairo
 import math
 
-class SharpieRenderer():
+class CairoPathRenderer():
     def __init__(self):
-        self.canvas = np.zeros((CANVAS_SIZE, CANVAS_SIZE, 4), dtype=np.uint8)
-        surface = cairo.ImageSurface.create_for_data(self.canvas, cairo.FORMAT_ARGB32, CANVAS_SIZE, CANVAS_SIZE)
+        self.canvas = np.zeros((CANVAS_SIZE, CANVAS_SIZE), dtype=np.uint8)
+        surface = cairo.ImageSurface.create_for_data(self.canvas, cairo.FORMAT_A8, CANVAS_SIZE, CANVAS_SIZE)
         self.cr = cairo.Context(surface)
-        self.clear()
 
-    def draw(self, trajectory):
+    def draw(self, trajectory, thickness):
+        self.cr.set_source_rgba(0.0, 0.0, 0.0, 0.0)
+        self.cr.paint()
+
         path = []
         for i in range(len(trajectory)):
             path.append((
@@ -18,7 +20,7 @@ class SharpieRenderer():
                 CANVAS_SIZE * trajectory[i][1]
             ))
 
-        radius = 2
+        radius = thickness/2
         # draw trajectory path
         for i in range(len(path)-1):
             x1, y1 = path[i][0], path[i][1]
@@ -26,20 +28,14 @@ class SharpieRenderer():
             self.cr.set_line_width(2*radius)
             self.cr.move_to(x1, y1)
             self.cr.line_to(x2, y2)
-            self.cr.set_source_rgb(0, 0, 0)
+            self.cr.set_source_rgba(0.0, 0.0, 0.0, 1.0)
             self.cr.stroke()
         
         # draw circles along trajectory
         for i in range(len(path)):
             x, y = path[i][0], path[i][1]
             self.cr.arc(x, y, radius, 0, 2*math.pi)
-            self.cr.set_source_rgb(0, 0, 0)
+            self.cr.set_source_rgba(0.0, 0.0, 0.0, 1.0)
             self.cr.fill()
         
-        return self.canvas[:,:,:3] / 255
-
-    def clear(self):
-        self.cr.set_source_rgb(255, 255, 255)
-        self.cr.paint()
-
-        return self.canvas[:,:,:3] / 255
+        return self.canvas / 255
---
+++ src/renderers/diff_path_renderer.py
@@ -0,0 +1,42 @@
+import torch
+from torch import nn
+from constants import CANVAS_SIZE
+
+class DiffPathRenderer(nn.Module):
+    def __init__(self):
+        super(DiffPathRenderer, self).__init__()
+
+        idxs_x = torch.arange(CANVAS_SIZE)
+        idxs_y = torch.arange(CANVAS_SIZE)
+        x_coords, y_coords = torch.meshgrid(idxs_y, idxs_x, indexing='ij') # CANVAS_SIZE x CANVAS_SIZE
+        self.grid_coords = torch.stack((y_coords, x_coords), dim=2).reshape(1,CANVAS_SIZE,CANVAS_SIZE,2) # 1 x CANVAS_SIZE x CANVAS_SIZE x 2
+
+    def forward(self, traj, thickness):
+        traj = traj * CANVAS_SIZE
+        n = len(traj)
+
+        vs = traj[:-1].reshape((-1,1,1,2)) # (n-1, 1, 1, 2)
+        vs = torch.tile(vs, (1, CANVAS_SIZE, CANVAS_SIZE, 1)) # (n-1, CANVAS_SIZE, CANVAS_SIZE, 2)
+
+        ws = traj[1:].reshape((-1,1,1,2)) # (n-1, 1, 1, 2)
+        ws = torch.tile(ws, (1, CANVAS_SIZE, CANVAS_SIZE, 1)) # (n-1, CANVAS_SIZE, CANVAS_SIZE, 2)
+
+        coords = torch.tile(self.grid_coords, (n-1,1,1,1)).to(ws.device) # (n-1, CANVAS_SIZE, CANVAS_SIZE, 2)
+
+        # For each of the n-1 segments, compute distance from every point to the line
+        def dist_line_segment(p, v, w):
+            d = torch.linalg.norm(v-w, dim=3) # (n-1) x CANVAS_SIZE x CANVAS_SIZE
+            dot = (p-v) * (w-v)
+            dot_sum = torch.sum(dot, dim=3) / (d**2 + 1e-5)
+            t = dot_sum.unsqueeze(3) # (n-1) x CANVAS_SIZE x CANVAS_SIZE x 1
+            t = torch.clamp(t, min=0, max=1) # (n-1) x CANVAS_SIZE x CANVAS_SIZE x 1
+            proj = v + t * (w-v) # (n-1) x CANVAS_SIZE x CANVAS_SIZE x 2
+            return torch.linalg.norm(p-proj, dim=3)
+        distances = dist_line_segment(coords, vs, ws) # (n-1, CANVAS_SIZE, CANVAS_SIZE)
+        distances = torch.min(distances, dim=0).values
+
+        radius = thickness/2
+        darkness = torch.clamp((radius - distances) / radius, min=0.0, max=1.0)
+        darkness = darkness ** 0.2
+
+        return darkness
