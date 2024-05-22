c42078e17ea373c3d7f23888010724480962c292
ldcWV <ldorianchen@gmail.com>
2024-05-22 18:00:09 -0400

    Use images from dataset as goals

--- src/train.py
+++ src/train.py
@@ -8,18 +8,24 @@ import torch
 import cv2
 import matplotlib.pyplot as plt
 import wandb
+import numpy as np
 
-def sample_image(data_dir):
+def preload_images(data_dir):
     files = os.listdir(data_dir)
-    file = random.choice(files)
-    path = os.path.join(data_dir, file)
+    imgs = []
+    for file in files:
+        path = os.path.join(data_dir, file)
 
-    img = cv2.imread(path)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    img = cv2.resize(img, (CANVAS_SIZE, CANVAS_SIZE))
-    img = torch.from_numpy(img) / 255
+        img = cv2.imread(path)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img = cv2.resize(img, (CANVAS_SIZE, CANVAS_SIZE))
+        img = torch.from_numpy(img) / 255
+        imgs.append(img)
+    return torch.stack(imgs, dim=0)
 
-    return img
+def sample_images(imgs, batch_size):
+    idxs = np.random.choice(len(imgs), batch_size)
+    return imgs[idxs]
 
 def main(args):
     wandb.init(project="RLFrida", config=args, mode=args.wandb_mode)
@@ -30,9 +36,11 @@ def main(args):
     agent = DDPG(env)
     agent.set_device(device)
 
+    imgs = preload_images(args.data_dir)
+
     for episode_idx in range(args.num_episodes):
-        # goal = sample_image(args.data_dir).to(device)
-        goal = torch.ones((args.batch_size, CANVAS_SIZE, CANVAS_SIZE, 3))
+        goal = sample_images(imgs, args.batch_size).to(device)
+        # goal = torch.ones((args.batch_size, CANVAS_SIZE, CANVAS_SIZE, 3))
         # goal[:CANVAS_SIZE//2, :, :] = 0
         goal = goal.to(device)
         env.reset()
@@ -57,7 +65,7 @@ def main(args):
 
         wandb_dict = {
             "episode": episode_idx,
-            "total reward": tot_reward.mean()
+            "return": tot_reward.mean()
         }
         if episode_idx % 10 == 0:
             for i in range(min(10, args.batch_size)):
@@ -74,14 +82,14 @@ def main(args):
                 done,
                 goal
             )
-            # agent.add_to_replay_buffer(
-            #     obs,
-            #     action,
-            #     env.calc_reward(obs, next_obs, hindsight_goal),
-            #     next_obs,
-            #     done,
-            #     hindsight_goal
-            # )
+            agent.add_to_replay_buffer(
+                obs,
+                action,
+                env.calc_reward(obs, next_obs, hindsight_goal),
+                next_obs,
+                done,
+                hindsight_goal
+            )
         
         if episode_idx >= args.num_warmup_episodes:
             for i in range(args.train_iters_per_episode):
@@ -91,13 +99,12 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--num_episodes', type=int, default=5000)
-    parser.add_argument('--num_warmup_episodes', type=int, default=10)
-    parser.add_argument('--batch_size', type=int, default=16)
+    parser.add_argument('--num_warmup_episodes', type=int, default=0)
+    parser.add_argument('--batch_size', type=int, default=96)
     parser.add_argument('--stroke_bundle_size', type=int, default=5)
-    parser.add_argument('--data_dir', type=str, default=r"C:\Users\Ldori\OneDrive\Desktop\RLFrida\data\ContourDrawingDataset")
+    parser.add_argument('--data_dir', type=str, default=r"/home/frida/lawrence/RLFrida/data/ContourDrawingDataset")
     parser.add_argument('--wandb_mode', type=str, default="disabled")
     parser.add_argument('--train_iters_per_episode', type=int, default=10)
 
     args = parser.parse_args()
-    with torch.autograd.detect_anomaly(True):
-        main(args)
+    main(args)
